d365368da5824605924f64b2ca537708a10ceb2b
MyFirstName MylastName <adriana21gt@gmail.com>
2024-12-25 23:28:14 -0600

    se completaron las secciones de read me, y app.py con botones y checkbox

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# analysis_vehicles_proy  breve descripción del proyecto, donde se explique para qué sirve la aplicación web y qué tipo de funcionalidad proporciona.
+# analysis_vehicles_proy  
+En esta pagina se despliega el analisis de datos de venta de autos, se muestran los datos asi como graficos de los principales parametros como son type, odometer, model year, price, con el fin de visualizar la distribucion de los datos.
+El link para la aplicacion es el siguiente:
+https://analysis-vehicles-proy.onrender.com
--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import plotly
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
-st.header('Dasb')
-st.write('mi primera data app!')
+# mostrar un encabezado
+st.header('Analisis de datos de anuncios de venta de coches')
+
+st.write('Encabezado de los datos')
+st.dataframe(car_data.head())  # imprimir primeros renglones
 
 # crear un botón para histograma
-hist_button = st.button('Construir histograma')
+hist_button_1 = st.button('Construir histograma de tipo de auto')
 
-if hist_button:  # al hacer clic en el botón
+if hist_button_1:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="type")
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
 # crear un botón para histograma
-scatter_button = st.button('Construir gráfico de dispersión')
+scatter_button = st.button('Construir gráfico de dispersión de kilometraje')
 
 if scatter_button:  # al hacer clic en el botón
     # escribir un mensaje
@@ -35,13 +39,30 @@ if scatter_button:  # al hacer clic en el botón
     st.plotly_chart(fig2, use_container_width=True)
 
 # crear una casilla de verificación
-build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir un diagrama de dispersion de modelo de auto')
 
-if build_histogram:  # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
+if build_scatter:  # si la casilla de verificación está seleccionada
+    st.write('Diagrama de dispersión de modelo de auto')
 
     # crear un graf de dispersion
     fig3 = px.scatter(car_data, x="model_year")
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig3, use_container_width=True)
+
+
+build_histogram_2 = st.checkbox('Construir un histograma de precio')
+
+if build_histogram_2:
+    st.write('histograma para el precio')
+    fig4 = px.histogram(car_data, x="price")
+    st.plotly_chart(fig4, use_container_width=True)
+
+
+# git status  para ver todos los cambios
+# git add . para agregar todos los cambios
+# git commit -m "mensaje" para hacer un commit
+# git push para subir los cambios
+# git pull para bajar los cambios
+# git clone para clonar un repositorio
+# git branch para ver las ramas
